ce743db0990e7db3cadee3e9cb42a516e9de61d0
James Gould <nobleburgundy@gmail.com>
2020-10-20 09:13:24 -0500

    Added some calculations for score, more questions, and 'worth' property to question.

--- assets/game.js
+++ assets/game.js
@@ -16,6 +16,7 @@ let answer4Element = document.querySelector("button#answer4");
 let currentQuestionId = 1;
 let secondsLeft = 60;
 let score = 0;
+let correctAnswerCount = 0;
 
 let timerElement = startButton.addEventListener("click", function (event) {
   event.preventDefault();
@@ -45,7 +46,7 @@ answerDiv.addEventListener("click", function (event) {
   event.preventDefault();
   if (event.target.id === getCorrectAnswer(currentQuestionId)) {
     console.log("Correct!");
-    score += 10;
+    correctAnswerCount++;
     updateScore();
   } else {
     console.log("Incorrect");
@@ -64,19 +65,37 @@ function getCorrectAnswer(questionId) {
   return questions[questionId - 1].question.correctAnswer;
 }
 
+function getTotalPotentialScore() {
+  let total = 0;
+  for (let index = 0; index < questions.length; index++) {
+    total += questions[index].worth;
+  }
+  return total;
+}
+
+function getScorePercentage() {
+  return ((correctAnswerCount / questions.length) * 100).toFixed(0);
+}
+
 function updateScore() {
+  score += questions[currentQuestionId - 1].worth;
   scoreElement.textContent = score;
 }
 
 function endGame() {
-  console.log(`GAME OVER\nYOUR SCORE: ${score}`);
-  alert(`GAME OVER\nYOUR SCORE: ${score}`);
+  alert(
+    `GAME OVER\nYOUR SCORE: ${score}\nAnswered ${correctAnswerCount} out of ${
+      questions.length
+    } (% ${getScorePercentage()})`
+  );
+
+  //pop modal with score info, save high score with initials, and restart button?
 }
 
 function updateQuestion(questionId) {
   let questionData = questions[questionId - 1];
 
-  questionTextElement.textContent = questionData.question.questionText;
+  questionTextElement.innerHTML = questionData.question.questionText;
   answer1Element.textContent = questionData.question.answer1;
   answer2Element.textContent = questionData.question.answer2;
   answer3Element.textContent = questionData.question.answer3;
@@ -96,6 +115,7 @@ let questions = [
       answer4: "Java Source Oriented Nodes",
       correctAnswer: "answer3",
     },
+    worth: 10,
   },
   {
     id: 2,
@@ -107,6 +127,7 @@ let questions = [
       answer4: "the HEAD section of the HTML file where this function is used.",
       correctAnswer: "answer1",
     },
+    worth: 10,
   },
   {
     id: 3,
@@ -118,6 +139,7 @@ let questions = [
       answer4: "<meta charset='UTF-8'>",
       correctAnswer: "answer4",
     },
+    worth: 10,
   },
   {
     id: 4,
@@ -129,5 +151,92 @@ let questions = [
       answer4: "North, South, East, West",
       correctAnswer: "answer3",
     },
+    worth: 10,
+  },
+  {
+    id: 5,
+    question: {
+      questionText: "What does CSS stand for?",
+      answer1: "Cool Site Styles",
+      answer2: "Cascading Style Sheets",
+      answer3: "Cats Sound Silly",
+      answer4: "Cascading Style Sources",
+      correctAnswer: "answer2",
+    },
+    worth: 10,
+  },
+  {
+    id: 5,
+    question: {
+      questionText: "What prefix symbol is used to select an element by <code>id</code> in a CSS file?",
+      answer1: "#",
+      answer2: ".",
+      answer3: "?",
+      answer4: "$",
+      correctAnswer: "answer1",
+    },
+    worth: 10,
+  },
+  {
+    id: 6,
+    question: {
+      questionText: "What prefix symbol is used to select an element by <code>class</code> in a CSS file?",
+      answer1: "#",
+      answer2: ".",
+      answer3: "?",
+      answer4: "$",
+      correctAnswer: "answer2",
+    },
+    worth: 10,
+  },
+  {
+    id: 7,
+    question: {
+      questionText: "Which of the following is true?",
+      answer1: "1 === '1'",
+      answer2: "1 == null",
+      answer3: "1 === true",
+      answer4: "1 !== true",
+      correctAnswer: "answer4",
+    },
+    worth: 10,
+  },
+  {
+    id: 8,
+    question: {
+      questionText: "What is returned by the following: <code>Math.floor(Math.random() * 5)</code>?",
+      answer1: "Random number between 1 and 5",
+      answer2: "Math",
+      answer3: "Random number between 0 and 5",
+      answer4: "Random number between 5 and infinity",
+      correctAnswer: "answer3",
+    },
+    worth: 10,
+  },
+  {
+    id: 9,
+    question: {
+      questionText:
+        "What is logged to the console by the following: <pre><code>let x = 0.1; \nlet y = 'test';\nconsole.log(x + y);</code></pre>",
+      answer1: "Undefined",
+      answer2: "0.1test",
+      answer3: "Null",
+      answer4: "Nan",
+      correctAnswer: "answer2",
+    },
+    worth: 10,
+  },
+  {
+    id: 10,
+    question: {
+      questionText:
+        "The following will return what? <pre><code>let x = 2, y = 140;\nif (x > 1 || y < 100) { \n  return true; \n} else { \n  return false; \n}</code></pre>",
+      answer1: "true",
+      answer2: "false",
+      answer3: "140",
+      answer4: "2",
+      correctAnswer: "answer1",
+    },
+    worth: 10,
   },
 ];
