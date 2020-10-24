1e593491d2fd02bb20f3dd908677651c09842728
James Gould <nobleburgundy@gmail.com>
2020-10-24 10:40:48 -0500

    Added message for no high scores and added citation for sort array of objs.

--- assets/highscores.js
+++ assets/highscores.js
@@ -1,39 +1,57 @@
 function loadHighScores() {
   let highscores = JSON.parse(localStorage.getItem("high-scores"));
+  if (!highscores) {
+    highscores = [];
+  }
   return highscores;
 }
 
 function clearHighScores() {}
 
+// Citation for sorting array of objects:
+// https://www.javascripttutorial.net/array/javascript-sort-an-array-of-objects/
 function sortHighScores() {
   let newAr = loadHighScores();
-
-  newAr.sort(function (a, b) {
-    return b.score - a.score;
-  });
-
-  return newAr;
+  // Only sort if there are more than one element in the array
+  if (newAr.length > 1) {
+    newAr.sort(function (a, b) {
+      return b.score - a.score;
+    });
+  } else {
+    return newAr;
+  }
 }
 
 function setHighScores(max = 10) {
-  let highscores = sortHighScores();
+  let highScoresSorted = sortHighScores();
   let tableBody = document.querySelector("tbody");
-  for (let i = 0; i < max; i++) {
-    // just set the max ranking
-    let tr = document.createElement("tr");
-    let td_rank = document.createElement("td");
-    let td_initials = document.createElement("td");
-    let td_score = document.createElement("td");
-    let td_date = document.createElement("td");
-    td_rank.textContent = i + 1;
-    td_initials.textContent = highscores[i].initials;
-    td_score.textContent = highscores[i].score;
-    td_date.textContent = highscores[i].date;
-    tableBody.appendChild(tr);
-    tr.appendChild(td_rank);
-    tr.appendChild(td_initials);
-    tr.appendChild(td_score);
-    tr.appendChild(td_date);
+  // Set max to highScores array length if less than initial setting
+  if (max > highScoresSorted.length) {
+    max = highScoresSorted.length;
+  }
+  if (highScoresSorted.length > 0) {
+    for (let i = 0; i < max; i++) {
+      // just set the max ranking
+      let tr = document.createElement("tr");
+      let td_rank = document.createElement("td");
+      let td_initials = document.createElement("td");
+      let td_score = document.createElement("td");
+      let td_date = document.createElement("td");
+      td_rank.textContent = i + 1;
+      td_initials.textContent = highScoresSorted[i].initials;
+      td_score.textContent = highScoresSorted[i].score;
+      td_date.textContent = highScoresSorted[i].date;
+      tableBody.appendChild(tr);
+      tr.appendChild(td_rank);
+      tr.appendChild(td_initials);
+      tr.appendChild(td_score);
+      tr.appendChild(td_date);
+    }
+  } else {
+    let table = document.querySelector("table");
+    let noScoresMessage = document.createElement("p");
+    noScoresMessage.textContent = "No high scores have been set yet.";
+    table.parentElement.replaceChild(noScoresMessage, table);
   }
 }
 
